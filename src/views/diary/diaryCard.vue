b8eeceeae6e270b1ef1e357b52e8a718b47c2987
dauren0260 <violet0260@gmail.com>
2023-09-17 00:40:02 +0800

    新增檔案

---
+++ src/views/diary/diaryCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="diaryCenter">
+    <div class="diarySolid">
+      <a href="./diaryInside">
+        <div class="imgContent">
+          <div class="diaryTitleA">
+            <div class="diaryTitleFlex">
+              <img src="../../assets/img/member/member/1-suizukaNor.png" />
+              <p class="userName">哈洽馬</p>
+            </div>
+            <p class="userText">潛水體驗去</p>
+          </div>
+          <div>
+            <img src="../../assets/img/diary/diaryphoto1.png" />
+          </div>
+          <div class="diaryTitleB">
+            <p>綠島-鋼鐵礁</p>
+          </div>
+        </div>
+
+        <div class="diaryText">
+          <div class="diaryData">
+            <div>日誌類型<span>課程</span></div>
+            <div>潛水時間<span>45min</span></div>
+          </div>
+          <div class="diaryTextP">
+            <p>海風的味道，就像夏天的擁抱...</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'diaryCard',
+};
+</script>
+
+<style lang="scss" src="../../assets/scss/diaryCard.scss" scoped></style>
---
+++ src/views/diary/diaryInside.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>diaryInside的頁面</div>
+</template>
+
+<script>
+export default {
+    name:'diaryInside'
+}
+</script>
+
+<style>
+
+</style>
